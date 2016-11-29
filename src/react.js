2e73b88660895ffb74a927703ecabbfe47a2c1d0
Jorgen Evens <jorgen@evens.eu>
2016-11-29 11:22:57 +0100

    Add react 0.14 compatibility

--- src/react.js
+++ src/react.js
@@ -1,7 +1,8 @@
-/* global React */
 /* jshint esnext:true */
 
 // TODO: Remove the global `React` binding lookup once the ES6 Module Transpiler
 // supports external modules. This is a hack for now that provides the local
 // modules a referece to React.
+const React = require('react');
+
 export default React;
