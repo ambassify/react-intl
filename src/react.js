af67e22e9d2ba15143944d5280a0a18a63da60d3
Jorgen Evens <jorgen@evens.eu>
2016-11-30 23:25:38 +0100

    Don't use const, use var instead.

--- src/react.js
+++ src/react.js
@@ -1,8 +1,5 @@
 /* jshint esnext:true */
 
-// TODO: Remove the global `React` binding lookup once the ES6 Module Transpiler
-// supports external modules. This is a hack for now that provides the local
-// modules a referece to React.
-const React = require('react');
+var React = require('react');
 
 export default React;
